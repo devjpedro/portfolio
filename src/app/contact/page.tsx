959ff6a161c38fcc56a97cf20f16c5bb4bc93d5b
devjpedro <joaopedross.ti9@gmail.com>
2024-07-26 22:05:23 -0300

    fix prettier errors

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,11 +2,11 @@
 
 import { Button } from '@/components/ui/button'
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
